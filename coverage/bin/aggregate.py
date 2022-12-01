7bc2f982a7c7f7bc904dc144398ad3d49154c93e
Mohadese Sayahian Dehkordi <71869685+mohadesesd@users.noreply.github.com>
2022-12-01 09:10:38 -0500

    Update aggregate.py

--- coverage/bin/aggregate.py
+++ coverage/bin/aggregate.py
@@ -10,10 +10,12 @@ from statistics import mean, median
 argparser = argparse.ArgumentParser(description = 'Aggregate depth information (output as JSON) from individual depth files (generated using SAMtools mpileup).')
 argparser.add_argument('-i', '--in', metavar = 'file', dest = 'in_files_list', required = True, help = 'Input file which lists all depth files (one depth file per sample) generated using SAMtools mpileup. One file per line.')
 argparser.add_argument('-o', '--out', metavar = 'file', dest = 'out_file_name', required = True, help = 'Output file of depth information compressed with bgzip. In addition to this file, the tabix index will be produced.')
-argparser.add_argument('-f', '--flag', metavar = 'string', dest = 'output_type_flag', required = True, help = 'Flag identifies the type of output file whether it is a json file or a tsv file. Flag value should be either \'j\' indicate json format or \'t\' indicate tsv format.')
+argparser.add_argument('-f', '--format', metavar = 'string', dest = 'output_format', required = True, default = 't', help = 'The type of output file: \'j\' - JSON, \'t\' - TSV (default).')
 
 if __name__ == '__main__':
     args = argparser.parse_args()
+    if args.output_format not in {'t', 'j'}:
+        raise Error('')
     file_names = []
     with open(args.in_files_list, 'r') as ifile:
         for line in ifile:
@@ -27,8 +29,9 @@ if __name__ == '__main__':
     breaks = [1, 5, 10, 15, 20, 25, 30, 50, 100]
     with ExitStack() as stack, pysam.BGZFile(args.out_file_name, 'w') as ofile:
         ifiles = [ stack.enter_context(gzip.open(file_name, 'rt')) for file_name in file_names ]
-        if(args.output_type_flag == 't'):
-                ofile.write('#CHROM\tBP\tMEAN\tMEDIAN\tPCT_INDV_OVER_1X\tPCT_INDV_OVER_5X\tPCT_INDV_OVER_10X\tPCT_INDV_OVER_15X\tPCT_INDV_OVER_20X\tPCT_INDV_OVER_25X\tPCT_INDV_OVER_30X\tPCT_INDV_OVER_50X\tPCT_INDV_OVER_100X\n'.encode())
+        if (args.output_format == 't'):
+            #ofile.write('#CHROM\tBP\tMEAN\tMEDIAN\tPCT_INDV_OVER_1X\tPCT_INDV_OVER_5X\tPCT_INDV_OVER_10X\tPCT_INDV_OVER_15X\tPCT_INDV_OVER_20X\tPCT_INDV_OVER_25X\tPCT_INDV_OVER_30X\tPCT_INDV_OVER_50X\tPCT_INDV_OVER_100X\n'.encode())
+            ofile.write('#CHROM\tBP\tMEAN\tMEDIAN\t{}\n'.format('\t'.join([f'PCT_INDV_OVER_{br}X' for br in breaks])))
         while True:
             for i, ifile in enumerate(ifiles):
                 line = ifile.readline()
@@ -47,7 +50,7 @@ if __name__ == '__main__':
                 for i in range(0, len(breaks)):
                     if dp >= breaks[i]:
                         counts[i] += 1
-            if(args.output_type_flag == 't'):
+            if(args.output_format == 't'):
                 ofile.write('{}\t{:d}\t{:g}\t{:g}'.format(chromosome.replace('chr', '', 1), min_position, mean(depths), median(depths)).encode())
             else:
                 ofile.write('{}\t{:d}\t{:d}\t{{"chrom":"{}","start":{:d},"end":{:d},"mean":{:g},"median":{:g}'.format(chromosome.replace('chr', '', 1), min_position, min_position, chromosome.replace('chr', '', 1), min_position, min_position, mean(depths), median(depths)).encode())
