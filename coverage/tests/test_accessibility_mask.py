7077ac735c01d9db8247ebf4c9f48a69db448406
mohadesesd <mohadese.sayahian.d@gmail.com>
2022-12-13 16:09:50 -0500

    update

--- coverage/tests/test_accessibility_mask.py
+++ coverage/tests/test_accessibility_mask.py
@@ -27,30 +27,71 @@ class TestInterval(unittest.TestCase):
         make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
         with open(truth_path) as truth, open(output_path) as output:
             self.assertListEqual(list(truth), list(output))
+    
+    def test_one_no_region_one_large_region(self):
+        input_path = f'{self.test_data_path}/test3.txt.gz'
+        truth_path = f'{self.test_data_path}/truth3.bed'
+        output_path = 'test3.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))
 
+    def test_one_large_region_one_no_region(self):
+        input_path = f'{self.test_data_path}/test4.txt.gz'
+        truth_path = f'{self.test_data_path}/truth4.bed'
+        output_path = 'test4.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))    
+    
+    def test_one_no_region_one_region_one_no_region(self):
+        input_path = f'{self.test_data_path}/test5.txt.gz'
+        truth_path = f'{self.test_data_path}/truth5.bed'
+        output_path = 'test5.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))
+            
+    def test_one_region_one_no_region_one_region(self):
+        input_path = f'{self.test_data_path}/test6.txt.gz'
+        truth_path = f'{self.test_data_path}/truth6.bed'
+        output_path = 'test6.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))
+            
+    def test_two_non_continuous_regions(self):
+        input_path = f'{self.test_data_path}/test7.txt.gz'
+        truth_path = f'{self.test_data_path}/truth7.bed'
+        output_path = 'test7.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))
+    
+    def test_three_non_continuous_regions(self):
+        input_path = f'{self.test_data_path}/test8.txt.gz'
+        truth_path = f'{self.test_data_path}/truth8.bed'
+        output_path = 'test8.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))
+    
+    def test_two_non_continuous_regions_one_no_region(self):
+        input_path = f'{self.test_data_path}/test9.txt.gz'
+        truth_path = f'{self.test_data_path}/truth9.bed'
+        output_path = 'test9.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))
+    
+    def test_one_no_region_two_non_continuous_regions(self):
+        input_path = f'{self.test_data_path}/test10.txt.gz'
+        truth_path = f'{self.test_data_path}/truth10.bed'
+        output_path = 'test10.bed'
+        make_bed(self.column_name, input_path, output_path, self.min_percent, self.max_depth)
+        with open(truth_path) as truth, open(output_path) as output:
+            self.assertListEqual(list(truth), list(output))
+            
     def tearDown(self):
         for f in glob.glob('test*.bed'):
             os.remove(f)
-
-
-'''    
-    def testinterval(self):
-        input_names = glob.glob("test/*.txt.gz")
-        output_names = [i[:-7]+"_out.txt" for i in input_names]
-        for i in range(len(input_names)):
-            input_path = input_names[i]
-            output_path = "test/test_run.txt"
-            column_name = "PCT_INDV_OVER_10X"
-            min_percent = 0.85
-            max_depth = 100
-            test_out = output_names[i]
-            make_bed(column_name, input_path, output_path, min_percent, max_depth)
-            print("testing " + input_names[i][5:-7])
-            self.assertListEqual(
-                list(io.open(test_out)),
-                list(io.open(output_path)))
-            print(input_names[i][5:-7] +" PASS!")
-''' 
-
-if __name__ == '__main__':
-    unittest.main()
